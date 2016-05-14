5b5bdaf58d56ee5649d1ed681d59f5238c1b779a
jaycubstech <imwithsteupid@gmail.com>
2016-05-14 15:14:15 -0700

    Add details in controller.js.

--- server/router.js
+++ server/router.js
@@ -5,5 +5,11 @@ module.exports = function(app){
   var controller = require('./controller.js');
 
   //Define the routes
-  app.route('/').get('controller.index');
+  app.route('/').get(controller.index);
+  app.route('/game:id').get(controller.getGame);
+  app.route('/game').post(controller.createGame);
+  app.route('/game:id').delete(controller.deleteGame);
+  app.route('/game').put(controller.updateGame);
+  app.route('/page:id').get(controller.displayGame);
+  app.route('/page').get(controller.showList)
 }
