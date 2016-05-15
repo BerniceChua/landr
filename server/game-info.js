4bd45a67975455701cb39e950ea2e5aa439d099c
BerniceChua <Bernice.Chua.415@gmail.com>
2016-05-15 12:32:30 -0700

    Edit server/game-info.js to obscure our password.

--- server/game-info.js
+++ server/game-info.js
@@ -1,7 +1,9 @@
+require('dotenv').config();
 var mongoose = require('mongoose');
 
+let mongooseURI = 'mongodb://' + DB_USER + ':' + DB_PASSWORD + '@ds036069.mlab.com:36069/LandrDB';
 //mongoose.connect('mongodb://localhost:8080/my_database');
-mongoose.connect('mongodb://landr-hackathon:Hackathon-2016-landR@ds036069.mlab.com:36069/LandrDB');
+mongoose.connect(mongooseURI);
 
 var Schema = mongoose.Schema;
 
