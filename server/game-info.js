db1b73fe0c779ff182fa1343f8518056cd3e9c5f
François Guézengar <Franois Guzengar>
2016-05-15 13:46:39 -0700

    template

--- server/game-info.js
+++ server/game-info.js
@@ -1,8 +1,8 @@
 //require('dotenv').config();
 var mongoose = require('mongoose');
 
-//let mongooseURI = 'mongodb://' + DB_USER + ':' + DB_PASSWORD + '@ds036069.mlab.com:36069/LandrDB';
-//mongoose.connect(mongooseURI);
+let mongooseURI = 'mongodb://' + DB_USER + ':' + DB_PASSWORD + '@ds036069.mlab.com:36069/LandrDB';
+mongoose.connect(mongooseURI);
 
 var Schema = mongoose.Schema;
 
